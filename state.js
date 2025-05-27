778cce58e33acb8775f2669dee71b4ab737eba86
Jai Bellare <jaibellare@gmail.com>
2025-05-27 11:06:04 +0200

    head angle warning attempt

--- state.js
+++ state.js
@@ -1,17 +1,14 @@
 class State {
-  constructor() {
-    // this.poses = [];
-  }
-
   preload() {
     // Load the bodyPose model
     console.log("Loading BodyPose model...");
-    this.bodyPose = ml5.bodyPose();
+    this.bodyPose = ml5.bodyPose("BlazePose");
     console.log("done");
   }
 
   setup() {
-    createCanvas(windowWidth, windowHeight);
+    // createCanvas(windowWidth, windowHeight);
+    createCanvas(1920, 1080);
 
     // using ideal: max is "not officially supported" according to ChatGPT, but it works in Chrome on Windows
     // In Firefox: DOMException: Failed to allocate videosource
@@ -34,16 +31,19 @@ class State {
     this.video.hide();
 
     this.infoElement = select("#info");
+    this.warningElement = select("#warning");
 
     // Start detecting poses in the webcam video
     this.bodyPose.detectStart(this.video, (poses) => {
-      this.poses = poses;
+      //   TODO could choose pose with highest confidence
+      this.pose = poses[0];
+      //   console.log(poses);
     });
 
     // Get the skeleton connection information
     this.connections = this.bodyPose.getSkeleton();
 
-    this.CONFIDENCE_THRESHOLD = 0.01;
+    this.CONFIDENCE_THRESHOLD = 0.1;
   }
 
   draw() {
@@ -68,58 +68,132 @@ class State {
 
     this.drawSkeleton();
 
+    this.detection();
+
     const dims = `video: ${this.video.width}x${this.video.height}\ncanvas: ${width}x${height}\nWindow: ${windowWidth}x${windowHeight}`;
-    this.infoElement.html(dims);
+    // this.infoElement.html(dims);
   }
 
   // TODO should implement CONTAIN functionality of image. its not a simple scaling.
   transformX(x) {
+    return x;
     return map(x, 0, this.video.width, 0, width);
   }
 
   transformY(y) {
+    return y;
     return map(y, 0, this.video.height, 0, height);
   }
 
   drawSkeleton() {
-    if (!this.poses) {
-      return; // No poses detected, nothing to draw
+    if (!this.pose) {
+      return; // No pose detected, nothing to draw
     }
 
-    for (const pose of this.poses) {
-      for (let j = 0; j < this.connections.length; j++) {
-        let pointAIndex = this.connections[j][0];
-        let pointBIndex = this.connections[j][1];
-        let pointA = pose.keypoints[pointAIndex];
-        let pointB = pose.keypoints[pointBIndex];
-        // Only draw a line if both points are confident enough
-        if (
-          pointA.confidence > this.CONFIDENCE_THRESHOLD &&
-          pointB.confidence > this.CONFIDENCE_THRESHOLD
-        ) {
-          stroke(255, 0, 0);
-          strokeWeight(2);
-          line(
-            this.transformX(pointA.x),
-            this.transformY(pointA.y),
-            this.transformX(pointB.x),
-            this.transformY(pointB.y)
-          );
-          //   console.log(pointA.x, pointA.y, pointB.x, pointB.y);
-        }
+    for (let j = 0; j < this.connections.length; j++) {
+      const pointAIndex = this.connections[j][0];
+      const pointBIndex = this.connections[j][1];
+      const pointA = this.pose.keypoints[pointAIndex];
+      const pointB = this.pose.keypoints[pointBIndex];
+      // Only draw a line if both points are confident enough
+      if (
+        pointA.confidence > this.CONFIDENCE_THRESHOLD &&
+        pointB.confidence > this.CONFIDENCE_THRESHOLD
+      ) {
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        line(
+          this.transformX(pointA.x),
+          this.transformY(pointA.y),
+          this.transformX(pointB.x),
+          this.transformY(pointB.y)
+        );
+        //   console.log(pointA.x, pointA.y, pointB.x, pointB.y);
       }
     }
 
     // Draw all the tracked landmark points
-    for (const pose of this.poses) {
-      for (let j = 0; j < pose.keypoints.length; j++) {
-        let keypoint = pose.keypoints[j];
-        if (keypoint.confidence > this.CONFIDENCE_THRESHOLD) {
-          fill(0, 255, 0);
-          noStroke();
-          circle(this.transformX(keypoint.x), this.transformY(keypoint.y), 10);
-        }
+    for (let j = 0; j < this.pose.keypoints.length; j++) {
+      const keypoint = this.pose.keypoints[j];
+      if (keypoint.confidence > this.CONFIDENCE_THRESHOLD) {
+        fill(0, 255, 0);
+        noStroke();
+        circle(this.transformX(keypoint.x), this.transformY(keypoint.y), 10);
       }
     }
+  }
+
+  keypointPos(name) {
+    if (!this.pose) {
+      return;
+    }
+
+    const keypoint_ = this.pose[name];
+    if (!keypoint_) {
+      return;
+    }
+
+    return createVector(
+      keypoint_.keypoint3D.x,
+      keypoint_.keypoint3D.y,
+      keypoint_.keypoint3D.z
+    );
+  }
+
+  vecToString(vec) {
+    return `(${vec.x.toFixed(2)}, ${vec.y.toFixed(2)}, ${vec.z.toFixed(2)})`;
+  }
+
+  detection() {
+    const left_ear = this.keypointPos("left_eye");
+    const right_ear = this.keypointPos("right_eye");
+    const nose = this.keypointPos("nose");
+
+    const left_shoulder = this.keypointPos("left_shoulder");
+    const right_shoulder = this.keypointPos("right_shoulder");
+
+    if (!(left_ear && right_ear && nose && left_shoulder && right_shoulder)) {
+      return;
+    }
+
+    // const v = p5.Vector.normalize(
+    //   nose.sub(left_ear.add(right_ear).div(2)).mult([1, 1, 0])
+    // );
+    const v = p5.Vector.normalize(left_ear.sub(right_ear).mult([1, 1, 0]));
+
+    const u = p5.Vector.normalize(
+      left_shoulder.sub(right_shoulder).mult([1, 1, 0])
+    );
+
+    // console.log(this.vecToString(u), this.vecToString(v));
+    // angleMode(DEGREES);
+    // const heading = v.heading();
+    const heading = u.angleBetween(v);
+
+    if (!this.min_heading && !this.max_heading) {
+      this.min_heading = heading;
+      this.max_heading = heading;
+    }
+    this.min_heading = min(this.min_heading, heading);
+    this.max_heading = max(this.max_heading, heading);
+    const x = map(heading, this.min_heading, this.max_heading, 0, 1);
+
+    let str = `Min: ${this.min_heading.toFixed(
+      2
+    )} | Max: ${this.max_heading.toFixed(2)} | Cur: ${heading.toFixed(
+      2
+    )} ${x.toFixed(2)}`;
+    let warning = "";
+
+    console.log(heading.toFixed(2));
+
+    if (x > 0.8 || x < 0.2) {
+      warning += "\nLook straight!";
+    } else {
+      warning += "";
+    }
+
+    this.infoElement.html(str);
+    this.warningElement.html(warning);
   }
 }
