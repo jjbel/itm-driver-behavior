e3928e36552e0618bbe5a1f6693ac659c1eb8776
Jai Bellare <jaibellare@gmail.com>
2025-06-04 13:07:14 +0200

    matlab: save model data to csv

---
+++ SharedVar.m
@@ -0,0 +1,7 @@
+classdef SharedVar < handle
+
+    properties
+        Value
+    end
+
+end
--- plot.m
+++ plot.m
@@ -2,9 +2,11 @@
 function plot
     u = udpport("datagram", "LocalHost", "0.0.0.0", "LocalPort", 5014);
 
-    cleanup = onCleanup(@cleanupFn);
+    % to be accessible in cleanupFn, needs to be passed by reference - use a handle
+    model_data_var = SharedVar();
+    model_data_var.Value = [];
 
-    data = [];
+    cleanup = onCleanup(@() cleanupFn(model_data_var));
 
     while true
 
@@ -18,17 +20,26 @@ function plot
                 value = typecast(uint8(datagram.Data(9:16)), 'double');
                 disp(datetime(timestamp / 1000, 'ConvertFrom', 'posixtime', 'TimeZone', 'Europe/Berlin'));
                 disp(value);
+
+                model_data_var.Value = [model_data_var.Value; [timestamp, value]];
             end
 
         end
 
-        pause(0.1); % avoid busy wait
+        % TODO needed else CtrlC doesnt call cleanupFn
+        pause(0.01); % avoid busy wait
     end
 
 end
 
-% if don't cleanup, matlab process keeps port open, can't reuse it
-function cleanupFn
+function cleanupFn(data)
+    % if don't cleanup, matlab process keeps port open, can't reuse it
     clear u
-    disp('closed socket.')
+    disp('closed socket.');
+
+    % this uses the same filename format as optirack, but appends " model" before .csv
+    csv_file = "Take " + string(datetime('now', 'TimeZone', 'local', 'Format', 'yyyy-MM-dd hh.mm.ss a')) + " model.csv"
+    disp(csv_file)
+    writematrix(data.Value, csv_file);
+    disp('exported csv.');
 end
